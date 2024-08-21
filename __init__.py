7d94b078bcc6328835be767c4cc2a23afae50c62
Stephen Forrest <115189353+mapleforrest@users.noreply.github.com>
2024-08-21 14:35:49 +0200

    Initial checkin of OpenMaple for Python

--- __init__.py
+++ __init__.py
@@ -1,25 +1,52 @@
 import sys
-import maple
 
-class LibraryInstance:
+if sys.version_info[0] != 3:
+    print("This script requires Python 3")
+    exit()
+elif not (sys.version_info >= (3,8)):
+    print("This script requires Python version >= 3.8")
+    exit()
 
-    def __getattr__(self, attr):
-        if(attr == '__spec__'):
-            return None
-        sess = maple.getactive()
-        if(sess):
-            sym = sess._wrap( sess._maplec.ToMapleName( sess._kv, bytes( attr, 'utf-8' ), True ) )
-        else:
-            sym = None
-        return sym
+from openmaple.Expression import Expression
+from openmaple.Session import Session
+from openmaple.importfrom import importfrom
+from openmaple.exportto import exportto
 
-    def __setattr__(self, attr, value):
-        if(attr == '__spec__'):
-            return None
-        sess = maple.getactive()
-        if(sess):
-            sym = sess._maplec.ToMapleName( sess._kv, bytes( attr, 'utf-8' ), True )
-            sess._maplec.MapleAssign( sess._kv, sym, sess._unwrap( value ) )
-        return None
+_activesession = Session()
 
-sys.modules[__name__] = LibraryInstance()
+def getactive():
+    global _activesession
+    return _activesession
+
+def setactive(sess):
+    global _activesession
+    _activesession = sess
+
+def eval(*args):
+    global _activesession
+    if(_activesession == None):
+        raise RuntimeError('cannot establish a connection to Maple')
+    return _activesession.eval(*args)
+
+def execute(a):
+    global _activesession
+    if(_activesession == None):
+        raise RuntimeError('cannot establish a connection to Maple')
+    return _activesession.execute(a)
+
+def range(a,b):
+    global _activesession
+    if(_activesession == None):
+        raise RuntimeError('cannot establish a connection to Maple')
+    return _activesession.range(a,b)
+
+def symbol(a):
+    global _activesession
+    if(_activesession == None):
+        raise RuntimeError('cannot establish a connection to Maple')
+    return _activesession.symbol(a)
+
+def symbols(*args):
+    if(_activesession == None):
+        raise RuntimeError('cannot establish a connection to Maple')
+    return _activesession.symbols(*args)
